21b7983830a89845219b3f909d77ce5238350c1a
Ahmed Haque <afhaque@rice.edu>
2015-04-07 23:28:20 -0500

    Uploaded packages

--- public/app.js
+++ public/app.js
@@ -4,8 +4,10 @@ jetbrains.controller("AppCtrl", function($http) {
     var app = this;
     var url = "http://localhost:3000";
 
-    app.saveProduct = function(newProduct) {
-        $http.post(url + "/add", {name:newProduct}).success(function() {
+
+    // Function for saving products. This takes a newProduct, establishes it's value as being the name
+    app.saveProduct = function(newProduct, newProductAuthor) {
+        $http.post(url + "/add", {name:newProduct, author: newProductAuthor}).success(function() {
             loadProducts(); // On success we are reloading the product list
         })
     };
--- server/server.js
+++ server/server.js
@@ -1,32 +1,43 @@
+// Node Server Logic
+//=======================================================
+
+// Dependencies
 var express = require('express');
-var app = express();
 var cors = require('cors');
 var bodyParser = require("body-parser");
+var mongoose = require('mongoose');
 
+// Creates the App
+var app = express();
 app.use(cors());
 app.use(bodyParser());
 
-var mongoose = require('mongoose');
+// Establishes the Mongoose Model to the Database
+
 mongoose.connect('mongodb://localhost/jetbrains');
+var Product = mongoose.model('Product', {name: String, author: String});
 
-var Product = mongoose.model('Product', {name: String});
-
+// Establishes the initial display of products
 app.get('/', function(req, res) {
     Product.find(function(err, products) {
         res.send(products);
     })
 })
 
+// Provides a function for adding entries - This gets called on from Angular upon hitting submit
 app.post("/add", function(req, res) {
     var name = req.body.name;
-    var product = new Product({name: name});
+    var author = req.body.author;
+    var product = new Product({name: name, author: author});
     product.save(function(err) {
         res.send();
     })
 })
 
+// Sets the local port to 3000
 app.listen(3000);
 
+// Initial code for adding in documents to the MongoDB database via mongoose
 /*
 var product = new Product({name: "Apache"});
 product.save(function(err) {
